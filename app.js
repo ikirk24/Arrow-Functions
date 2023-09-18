f8afc45ad4e3a8f4759ee1f68cc41b8d0aa74cae
Ian Kirk <iankirk24@gmail.com>
2023-09-18 14:54:35 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// 1. 
+// function double(arr) {
+//   return arr.map(function(val) {
+//     return val * 2;
+//   })
+// }
+//  Answer:
+const double = arr => arr.map(val => val *2);
+
+// 2. 
+// function squareAndFindEvens(numbers){
+//   var squares = numbers.map(function(num){
+//     return num ** 2;
+//   });
+//   var evens = squares.filter(function(square){
+//     return square % 2 === 0;
+//   });
+//   return evens;
+// }
+//  Answer:
+const squareAndFindEvens = numbers => numbers.map(num => num **2 ).filter(square => square % 2 === 0);
